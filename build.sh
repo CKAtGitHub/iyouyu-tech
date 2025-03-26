d39dfeb63a4660232546ffc959c3a5fbe9cbec21
huangbinglong9@gmail.com <h1002087870@gmail.com>
2025-03-26 23:47:37 +0800

    build: 优化 HTML 文件处理流程
    - 修改了对 HTML 文件中 footer 标签的处理逻辑
    - 增加了对 powerby section 的检查，避免重复插入
    - 使用 awk 替代 sed，提高了代码的可读性和灵活性
    - 优化了代码结构，提高了执行效率

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ hugo
 
 # Step 2: Find all HTML files in the public directory and its subdirectories
 find public -type f -name "*.html" | while read -r file; do
-    # Step 3: Check if the file contains a footer tag
-    if grep -q '</footer>' "$file"; then
-        # Step 4: Insert the specified HTML content before the footer tag
-        sed -i '' '/<\/footer>/i\
-<section class="powerby"><a href="http://beian.miit.gov.cn/" class="hover:text-primary-hover mx-1" target="_blank">粤ICP备2025390666号-1</a></section>' "$file"
+    # Step 3: Check if the file contains a footer tag and does not contain the powerby section
+    if grep -q '</footer>' "$file" && grep -q '<section class="powerby">' "$file"; then
+        # Step 4: Insert the specified HTML content before the last footer tag
+        awk '{a[NR]=$0} END {last_footer=0; for(i=1;i<=NR;i++) {if(a[i] ~ /<\/footer>/) {last_footer=i}} for(i=1;i<=NR;i++) {if(i==last_footer) {print "<section class=\"powerby\"><a href=\"http://beian.miit.gov.cn/\" class=\"hover:text-primary-hover mx-1\" target=\"_blank\">粤ICP备2025390666号-1</a></section>";} print a[i]}}' "$file" > temp && mv temp "$file"
     fi
 done
